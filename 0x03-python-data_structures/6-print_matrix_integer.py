2f32658603f25733b93684012eee9fd4ecfa6a2a
Soukaina <megdani20soukaina@gmail.com>
2024-03-20 01:41:44 +0100

    pycostyle

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for mat in matrix:
         for element in mat:
-            print("{:d}".format(element), end="" if mat[len(mat) - 1] == element else " ")
+            print("{:d}".format(element), end=""
+                  if mat[len(mat) - 1] == element else " ")
         print()
